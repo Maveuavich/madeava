7b0058d1d4282af6ebe77145a8daf74c1d4c1cb5
Maveuavich <m.defe_carther@icloud.com>
2026-07-01 16:44:25 -0400

    Add files via upload

---
+++ mother.cpp
@@ -0,0 +1,51 @@
+
+
+#include <iostream>
+
+
+
+
+int harryblzz() {
+    return 89;
+}
+
+void weeeener() {
+    int boncko{ 59 };
+    std::cout << harryblzz();
+    std::cout << "\n\n\n\n" << "glotgobp" << "\n\n\n\n";
+}
+
+int main()
+{
+
+    weeeener();
+
+    [[maybe_unused]] int maybeuse;
+
+    //cumstain
+
+    //api
+
+    int mother, father, jew;
+
+    std::cout << "\n\n\n";
+    double long patriot;
+    std::cin >> patriot;
+    std::cout << "\n\n\n";
+    
+    int player      { 9 };
+    const int player69    { 5 };
+    const int outcome = player + player69;
+
+    std::cout << outcome << "\n\n\n\n";
+
+    player = 5 + 2;
+    std::cout << player << "\n\n\n\n";
+    player = 69;
+    std::cout << player << "\n\n\n\n";
+    std::cout << "player chose:     " << patriot << "\n\n\n\n" << boncko;
+    
+
+
+}
+
